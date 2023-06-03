d0f00f7fc0572181332e0f570ddc71fb072ee6ad
JodiMcJodson <isekgood@gmail.com>
2023-06-03 10:51:15 -0500

    initial commit

---
+++ shell.nix
@@ -0,0 +1,16 @@
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    (pkgs.stdenv.mkDerivation rec {
+      name = "zig";
+      src = pkgs.fetchurl {
+        url = "https://ziglang.org/builds/zig-linux-x86_64-0.11.0-dev.3223+38b83d9d9.tar.xz";
+        sha256 = "NXc3H90QLhYJfcPufyr/IoE2USETuLPPkyDgooGqGGg=";
+      };
+      installPhase = ''
+        mkdir -p $out/bin
+        mv * $out/bin
+      '';
+    })
+  ];
+}
