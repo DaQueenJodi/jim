e71d07862acd869a5a0f9353f5379a2aadeb4166
JodiMcJodson <isekgood@gmail.com>
2023-06-06 05:51:17 -0500

    a

--- src/main.zig
+++ src/main.zig
@@ -5,6 +5,7 @@ const Input = zui.Input;
 const Allocator = std.mem.Allocator;
 const ArrayList = std.ArrayList;
 const TuiWindow = zui.TuiWindow;
+const TuiWriter = zui.TuiWriter;
 fn read_file(alloc: Allocator, path: []const u8) ![]u8 {
     const file = try fs.cwd().openFile(path, .{});
     defer file.close();
@@ -22,6 +23,7 @@ const Buffer = struct {
     data: []u8,
     lines: ArrayList(Line),
     allocator: Allocator,
+    window: TuiWindow,
     const Self = @This();
     pub fn fromFile(alloc: Allocator, path: []const u8) !Self {
         const data = try read_file(alloc, path);
@@ -40,6 +42,7 @@ const Buffer = struct {
             }
         }
         return .{ 
+            .window = try TuiWindow.init(alloc, 0, 0, 100, 50),
             .data = data,
             .lines = lines,
             .allocator = alloc
@@ -48,6 +51,9 @@ const Buffer = struct {
     pub fn deinit(self: *Self) void {
         self.allocator.free(self.data);
         self.lines.deinit();
+    }
+    pub fn draw(self: *Self, writer: *TuiWriter) !void {
+        try self.window.draw(self.allocator, writer);
     }
 };
 
@@ -60,13 +66,44 @@ fn digit_count(comptime T: type, int: T) usize {
     }
     return length;
 }
-
+const Cursor = struct {
+    x: usize,
+    y: usize,
+    constraint_x: usize,
+    constraint_y: usize,
+    const Self = @This();
+    pub fn move(self: *Self, x: usize, y: usize) void {
+        self.x = @min(self.constraint_x, x);
+        self.y = @min(self.constraint_y, y);
+    }
+    pub fn down(self: *Self) void {
+        self.y = @min(self.constraint_y, self.y + 1);
+    }
+    pub fn up(self: *Self) void {
+        if (self.y > 0) self.y -= 1;
+    }
+    pub fn left(self: *Self) void {
+        if (self.x > 0) self.x -= 1;
+    }
+    pub fn right (self: *Self) void {
+        self.x = @min(self.constraint_x, self.x + 1);
+    }
+};
 const JimCtx = struct {
     buffer: Buffer,
-    cursor: @Vector(2, usize),
+    cursor: Cursor,
     const Self = @This();
     pub fn fromFile(alloc: Allocator, path: []const u8) !Self {
-        return .{ .buffer = try Buffer.fromFile(alloc, path), .cursor = .{ 0, 0 } };
+        const buff = try Buffer.fromFile(alloc, path);
+        return .{ 
+            .buffer = buff,
+            .cursor = .{
+                .x = 0,
+                .y = 0,
+                .constraint_x = buff.window.size.x,
+                .constraint_y = buff.window.size.y
+            }
+        };
     }
     pub fn deinit(self: *Self) void {
         self.buffer.deinit();
@@ -77,23 +114,27 @@ pub fn main() !void {
     const allocator = gpa.allocator();
     var jimctx = try JimCtx.fromFile(allocator, "src/main.zig");
     defer jimctx.deinit();
-    var ctx = try zui.TuiCtx.init(allocator);
+    var ctx = try zui.TuiCtx.init();
     defer ctx.deinit();
     try ctx.start();
-    var window = try TuiWindow.init(allocator, 30, 10, 100, 100);
-    defer window.deinit();
-    try ctx.add_window(window);
-    window.printAt(.{}, 0, 0, jimctx.buffer.data[0..jimctx.buffer.lines.items[50].end]);
-    window.buff[0].attrs = .{ .bold = true };
-    window.buff[1].attrs = .{ .bold = false };
-    try ctx.draw_windows();
     while (true) {
+        jimctx.buffer.window.printAt(.{}, 0, 0, jimctx.buffer.data[0..jimctx.buffer.lines.items[5].end]);
+        jimctx.buffer.window.buff[
+            jimctx.buffer.window.coord_to_idx(
+                jimctx.cursor.x,
+                jimctx.cursor.y
+            )
+        ].attrs = .{ .reverse = true };
+        try ctx.writer.clear();
+        try jimctx.buffer.draw(&ctx.writer);
+        try ctx.writer.flush();
         const input = try ctx.get_input();
-        if (std.meta.eql(input, Input.fromStr("C-q"))) break;
-        if (std.meta.eql(input, Input.fromStr("C-n"))) jimctx.cursor[1] += 1;
-        if (std.meta.eql(input, Input.fromStr("C-p"))) jimctx.cursor[1] -= 1;
-        if (std.meta.eql(input, Input.fromStr("C-f"))) jimctx.cursor[0] += 1;
-        if (std.meta.eql(input, Input.fromStr("C-b"))) jimctx.cursor[0] -= 1;
+        const eql = std.meta.eql;
+        if (eql(input, Input.fromStr("C-q"))) break;
+        if (eql(input, Input.fromStr("C-n"))) jimctx.cursor.down();
+        if (eql(input, Input.fromStr("C-p"))) jimctx.cursor.up();
+        if (eql(input, Input.fromStr("C-f"))) jimctx.cursor.right();
+        if (eql(input, Input.fromStr("C-b"))) jimctx.cursor.left();
     }
     try ctx.stop();
 }
